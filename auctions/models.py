faa01a2514df9807c452d42c28f0aba62cd668d9
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-01-21 11:24:09 +0200

    add product model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Product(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description =models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.name} by {self.user.username}"
